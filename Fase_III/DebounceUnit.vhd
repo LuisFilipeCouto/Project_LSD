60c4e0c282ac3305af5c839c384b54eb53045b14
Luis Couto <70239504+LuisFilipeCouto@users.noreply.github.com>
2020-12-18 23:48:30 +0000

    Update DebounceUnit.vhd

--- Fase_III/DebounceUnit.vhd
+++ Fase_III/DebounceUnit.vhd
@@ -1,6 +1,3 @@
-------                                                          ------
------- Este código é o código fornecido na página da disciplina ------
-------                                                          ------
 library IEEE;
 use 	  IEEE.STD_LOGIC_1164.all;
 use     IEEE.NUMERIC_STD.all;
